d1ba16f91f3b64289d0fc1f119ab5b7ae8be7d50
Jose Carrera <jose.luiscarrera199526@outlook.com>
2023-07-25 13:31:50 -0600

    Changes to be committed: 	modified:   demoClase1/src/main/java/com/mycompany/democlase1/DemoClase1.java 	modified:   demoClase1/target/classes/com/mycompany/democlase1/DemoClase1.class

--- demoClase1/src/main/java/com/mycompany/democlase1/DemoClase1.java
+++ demoClase1/src/main/java/com/mycompany/democlase1/DemoClase1.java
@@ -13,32 +13,52 @@ import java.util.Scanner;
  * @author josel
  */
 public class DemoClase1 {
+    
+    public Map<Integer, Materia> asignacion;
+
+    // public Programa() {
+    //     asignacion =new HashMap<>();
+    // }
 
     public static void main(String[] args) {
+        
+        Integer option = 1;
+        
+        // Programa programa = new Programa();
+        
         //creacion de MAP
         Map<Integer, String> nuevoAlumno;
         nuevoAlumno = new HashMap<>();
         
-        //INPUT DEL USUSUARIO
-        System.out.println("Ingrese el nombre del nuevo alumno: ");
-        Scanner scanner = new Scanner(System.in);
-        String nvAlmnNombre = scanner.nextLine();
         
-        //PUT PARA EL MAP
-        nuevoAlumno.put(nuevoAlumno.size() + 1, nvAlmnNombre);
         
-        //CREACION DEL OBJETO USANDO MAP
-        Alumno alum = new Alumno(nuevoAlumno.size(), nuevoAlumno.get(nuevoAlumno.size()));
-        System.out.println("El alumno con la sigiente informacion ");
-        System.out.println("Carnet: " + alum.getId() );
-        System.out.println("Nombre: " + alum.getNombre());
-        
-        /*Revisando cambios*/
-        //CALCULOS
-        /* for (int i = 1; i <= nuevoAlumno.size(); i++) {
-        System.out.println(nuevoAlumno.get(i));
+        while (option < 3 ){
+            //INPUT DEL USUSUARIO
+            System.out.println("Ingrese el nombre del nuevo alumno: ");
+            Scanner nvoAlumno = new Scanner(System.in);
+            String nvAlmnNombre = nvoAlumno.nextLine();
+            
+            //PUT PARA EL MAP
+            nuevoAlumno.put(nuevoAlumno.size() + 1, nvAlmnNombre);
+            
+            //CREACION DEL OBJETO USANDO MAP
+            Alumno alum = new Alumno(nuevoAlumno.size(), nuevoAlumno.get(nuevoAlumno.size()));
+            System.out.println("El alumno con la sigiente informacion ");
+            System.out.println("Carnet: " + alum.getId() );
+            System.out.println("Nombre: " + alum.getNombre());
+            
+            System.out.println("Would you like to add another student?");
+            Scanner optionIn = new Scanner(System.in);
+            option = optionIn.nextInt();
+            
+            // System.out.println("Hello World!");*/
         }
 
-        System.out.println("Hello World!");*/
+        /*Revisando cambios*/
+            //CALCULOS
+            for (int i = 1; i <= nuevoAlumno.size(); i++) {
+            System.out.println(i + " " + nuevoAlumno.get(i));
+            }
+        
     }
 }
