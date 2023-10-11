b99f88225acbcc672577a9044842abd6a659c476
fernando2023 <fernandochial@gmail.com>
2023-10-11 17:32:10 -0600

    cambios en facturas det

---
+++ Web_app/ERP-JavaEE7/src/main/java/com/umgprogra/erp/ui/services/FacturaDetServicio.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umgprogra.erp.ui.services;
+
+import com.umgprogra.erp.javaee7UI.FacturasDET;
+import com.umgprogra.erp.util.JpaUtil;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author ferna
+ */
+public class FacturaDetServicio {
+
+    public boolean registroFacturaDet(Integer idFacturaDet, Integer cantidad, Integer precioUnitario, Integer iva, Integer idProducto) {
+
+        boolean exito = false;
+                
+        EntityManager entity = JpaUtil.getEntityManagerFactory().createEntityManager();
+
+        return exito;
+
+    }
+}
