d897e9b29f8ef3b2fd36577c8579bb187e21d8ec
fernando2023 <fernandochial@gmail.com>
2023-10-11 23:51:04 -0600

    cambio en facturas cab

--- Web_app/ERP-JavaEE7/src/main/java/com/umgprogra/erp/javaee7UI/FacturasDET.java
+++ Web_app/ERP-JavaEE7/src/main/java/com/umgprogra/erp/javaee7UI/FacturasDET.java
@@ -5,8 +5,13 @@
 package com.umgprogra.erp.javaee7UI;
 
 import com.umgprogra.erp.ui.services.FacturaDetServicio;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -14,21 +19,19 @@ import javax.faces.bean.SessionScoped;
  */
 @ManagedBean
 @SessionScoped
-public class FacturasDET {
+public class FacturasDET implements Serializable {
 
     private Integer idFacturaDet;
     private Integer cantidad;
     private double precioUnitario;
-    private double subtotal;
     private double iva;
     private Integer idProducto;
 
     //constructores
-    public FacturasDET(Integer idFacturaDet, Integer cantidad, double precioUnitario, double subtotal, double iva, Integer idProducto) {
+    public FacturasDET(Integer idFacturaDet, Integer cantidad, double precioUnitario, double iva, Integer idProducto) {
         this.idFacturaDet = idFacturaDet;
         this.cantidad = cantidad;
         this.precioUnitario = precioUnitario;
-        this.subtotal = subtotal;
         this.iva = iva;
         this.idProducto = idProducto;
     }
@@ -61,14 +64,6 @@ public class FacturasDET {
         this.precioUnitario = precioUnitario;
     }
 
-    public double getSubtotal() {
-        return subtotal;
-    }
-
-    public void setSubtotal(double subtotal) {
-        this.subtotal = subtotal;
-    }
-
     public double getIva() {
         return iva;
     }
@@ -89,17 +84,53 @@ public class FacturasDET {
 
     }
 
-    // FacturaCabUI idFac = new FacturaCabUI(); 
-//    public void RegistroFacturaDetalle() {
-//        Proveedores id = new Proveedores();
-//        FacturaDetServicio registroFac = new FacturaDetServicio();
-//        
-//        Integer idFacturaCab = id.getIdProveedor();
-//        
-//        registroFac.registroFacturaDet( idFacturaCab , this.cantidad, this.cantidad, this.cantidad, this.idProducto);
-//        
-//        
-//
-//    }
+    public void listaProducto() {
+        // Creación de lista
+        List<FacturasDET> ListProd = new ArrayList<>();
+
+        if (this.idProducto == 0 || this.precioUnitario == 0 || this.cantidad == 0 || this.iva == 0) {
+            // Algunos campos no se han completado correctamente, muestra un mensaje de error.
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Por favor, complete todos los campos correctamente.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        } else {
+            // Los campos se completaron correctamente, crea un nuevo producto y agrégalo a la lista.
+            FacturasDET producto = new FacturasDET();
+            // Asigna los valores de los campos a la instancia del producto
+            producto.setIdProducto(this.idProducto);
+            producto.setPrecioUnitario(this.precioUnitario);
+            producto.setCantidad(this.cantidad);
+            producto.setIva(this.iva);
+
+            // Agregar el producto a la lista
+            ListProd.add(producto);
+
+            // Reinicia los valores de los campos
+            this.idProducto = 0;
+            this.precioUnitario = 0;
+            this.cantidad = 0;
+            this.iva = 0;
+        }
+    }
+
+    public void registroFacturaDet() {
+
+        FacturaDetServicio registroFac = new FacturaDetServicio();
+
+        boolean exito = registroFac.registroFacturaDet(this.idFacturaDet, this.cantidad, this.precioUnitario, this.idProducto);
+
+        if (exito) {
+            // Éxito: muestra un mensaje de éxito
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Éxito", "Proveedor registrado con éxito");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        } else {
+            // Error: muestra un mensaje de error
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "No se pudo registrar el proveedor");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+        this.idProducto = null;
+        this.cantidad = null;
+        this.precioUnitario = 0;
+
+    }
 
 }
