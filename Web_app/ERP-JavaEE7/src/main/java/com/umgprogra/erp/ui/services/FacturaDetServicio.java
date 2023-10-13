d2bfad7845b5d52be730676ccbc9e81e6f77a56a
fernando2023 <fernandochial@gmail.com>
2023-10-13 00:12:53 -0600

    avance en factura detalle

--- Web_app/ERP-JavaEE7/src/main/java/com/umgprogra/erp/ui/services/FacturaDetServicio.java
+++ Web_app/ERP-JavaEE7/src/main/java/com/umgprogra/erp/ui/services/FacturaDetServicio.java
@@ -11,6 +11,7 @@ import com.umgprogra.erp.DAO.Facturadet;
 import com.umgprogra.erp.DAO.Inventario;
 import com.umgprogra.erp.util.JpaUtil;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
 /**
  *
@@ -18,43 +19,95 @@ import javax.persistence.EntityManager;
  */
 public class FacturaDetServicio {
 
-    public boolean registroFacturaDet(Integer idFactura, Integer cantidad, double precioUnitario, Integer idProducto) {
-
-        boolean exito = false;
-
-        double iva = precioUnitario * 0.12;
-
-        // Iniciar la persistencia
-        EntityManager entity = JpaUtil.getEntityManagerFactory().createEntityManager();
-
-        // Crear una instancia de Proveedor y configurar sus propiedades
-        Facturadet facDet = new Facturadet();
-        Inventario inventario = entity.find(Inventario.class, 1);
-        Facturacab facturaCab = entity.find(Facturacab.class, 1);
-
-        facDet.setIdfactura(facturaCab);
-        facDet.setCantidad(cantidad);
-        facDet.setPreciounitario(precioUnitario);
-        facDet.setIva(iva);
-        facDet.setIdproducto(inventario);
-        // Iniciar la transacción
-        entity.getTransaction().begin();
+//    public boolean registroFacturaDet(Integer idFactura, Integer cantidad, double precioUnitario, Integer idProducto) {
+//
+//        boolean exito = false;
+//
+//        double iva = precioUnitario * 0.12;
+//
+//        // Iniciar la persistencia
+//        EntityManager entity = JpaUtil.getEntityManagerFactory().createEntityManager();
+//
+//        // Crear una instancia de Proveedor y configurar sus propiedades
+//        Facturadet facDet = new Facturadet();
+//        Inventario inventario = entity.find(Inventario.class, 1);
+//        Facturacab facturaCab = entity.find(Facturacab.class, 1);
+//
+//        facDet.setIdfactura(facturaCab);
+//        facDet.setCantidad(cantidad);
+//        facDet.setPreciounitario(precioUnitario);
+//        facDet.setIva(iva);
+//        facDet.setIdproducto(inventario);
+//        // Iniciar la transacción
+//        entity.getTransaction().begin();
+//
+//        try {
+//            // Persistir la entidad
+//            entity.persist(facDet);
+//
+//            // Commit de la transacción
+//            entity.getTransaction().commit();
+//             actualizarInventario(idProducto, cantidad);
+//            exito = true;  // Si llegamos aquí, la operación fue exitosa
+//        } catch (Exception e) {
+//            // En caso de excepción, marcamos la operación como fallida
+//            entity.getTransaction().rollback();
+//            exito = false;
+//        } finally {
+//            entity.close();
+//        }
+//
+//        return exito;  // Devuelve true si la operación fue exitosa, de lo contrario, false
+//    }
+    public boolean registroFacturaDet(Integer idFactura, Integer cantidad, double precioUnitario, int idProducto) {
+        EntityManager entityManager = JpaUtil.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
 
         try {
-            // Persistir la entidad
-            entity.persist(facDet);
+            transaction.begin();
 
-            // Commit de la transacción
-            entity.getTransaction().commit();
-            exito = true;  // Si llegamos aquí, la operación fue exitosa
+            double iva = precioUnitario * 0.12;
+
+            // Crear una instancia de Facturadet y configurar sus propiedades
+            Facturadet facDet = new Facturadet();
+            Inventario inventario = entityManager.find(Inventario.class, idProducto);
+            Facturacab facturaCab = entityManager.find(Facturacab.class, 1);
+
+            facDet.setIdfactura(facturaCab);
+            facDet.setCantidad(cantidad);
+            facDet.setPreciounitario(precioUnitario);
+            facDet.setIva(iva);
+            facDet.setIdproducto(inventario);
+
+            // Persistir el detalle de factura
+            entityManager.persist(facDet);
+
+            // Actualizar el inventario
+            actualizarInventario(entityManager, idProducto, cantidad);
+            transaction.commit();
+            return true; // La operación fue exitosa
         } catch (Exception e) {
-            // En caso de excepción, marcamos la operación como fallida
-            entity.getTransaction().rollback();
-            exito = false;
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
         } finally {
-            entity.close();
+            entityManager.close();
         }
+    }
 
-        return exito;  // Devuelve true si la operación fue exitosa, de lo contrario, false
+    //funcion para actualizar la cantidad del inventario
+    private void actualizarInventario(EntityManager entityManager, Integer idProducto, Integer cantidad) {
+        Inventario inventario = entityManager.find(Inventario.class, idProducto);
+        if (inventario != null) {
+            int cantidadActual = inventario.getCantidad();
+            if (cantidadActual >= cantidad) {
+                inventario.setCantidad(cantidadActual - cantidad);
+                entityManager.merge(inventario);
+            } else {
+                // Manejo de error
+            }
+        }
     }
 }
