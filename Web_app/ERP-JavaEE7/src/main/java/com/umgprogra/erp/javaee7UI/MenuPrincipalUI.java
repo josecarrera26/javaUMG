1a3f0e018b404f28ed3273d254197ab2145867f1
Jose Carrera <jose.luiscarrera199526@outlook.com>
2023-10-23 15:34:00 -0600

    Actualizacion de main menu pop up funcionando

--- Web_app/ERP-JavaEE7/src/main/java/com/umgprogra/erp/javaee7UI/MenuPrincipalUI.java
+++ Web_app/ERP-JavaEE7/src/main/java/com/umgprogra/erp/javaee7UI/MenuPrincipalUI.java
@@ -4,12 +4,11 @@
  */
 package com.umgprogra.erp.javaee7UI;
 
-import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.inject.Named;
 import org.primefaces.PrimeFaces;
 import org.primefaces.model.DialogFrameworkOptions;
+import java.io.Serializable;
 
 /**
  *
@@ -17,18 +16,20 @@ import org.primefaces.model.DialogFrameworkOptions;
  */
 @ManagedBean
 @ViewScoped
-public class MenuPrincipalUI {
+public class MenuPrincipalUI implements Serializable{
     
     public MenuPrincipalUI() {
     }
 
     public void viewProducts() {
-            System.out.println("estas en mainmenu");
-            DialogFrameworkOptions options = DialogFrameworkOptions.builder()
-                    .resizable(false)
-                    .build();
+        System.out.println("estas en mainmenu");
+        DialogFrameworkOptions options = DialogFrameworkOptions.builder()
+                .resizable(true)
+                .contentHeight("100%")
+                .contentWidth("100%")
+                .build();
             System.out.println("Se crean las opciones del dialog");
-            PrimeFaces.current().dialog().openDynamic("sinacceso.xhtml", options, null);
+            PrimeFaces.current().dialog().openDynamic("sinacceso", options, null);
             System.out.println("Se crean las el dialog");
     }
 }
